bb71f3bb98194f47a7c51acfe002a86349ccb4b0
hichamdz38 <dachirhicham@gmail.com>
2020-12-02 10:48:45 +0100

    part 2: Templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user ={'name':'Amir','age':'26','class':'RTU'}
+	return render_template('index.html',
+	user=user)
